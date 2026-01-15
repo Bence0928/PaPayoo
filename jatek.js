61bf007824901ae7162e1974e4cc9eedf6b2f261
Bence0928 <74986128+Bence0928@users.noreply.github.com>
2026-01-15 17:18:13 +0100

    Add files via upload

--- jatek.js
+++ jatek.js
@@ -2,7 +2,6 @@ const params = new URLSearchParams(window.location.search);
 const JATEKOSOK_SZAMA = parseInt(params.get("jatekosok")) || 0;
 const CEL_PONTSZAM = 250;
 
-let globalisPontszam = 0; // A régi "PontSzamok" változó
 let mainContainer;
 
 let playersData = []
@@ -10,18 +9,24 @@ let playersData = []
 let maxRounds = 5;
 let currRound = 1;
 
+let roundLoserID = 0;
+
 function initialize() {
     mainContainer = document.getElementById("main");
-    renderPlayerNameInputs();
+    gameStart();
+}
+
+function gameStart(){
+    createPlayerNameInputs();
 }
 
 // 1. lépés: Játékos nevek bekérése
-function renderPlayerNameInputs() {
+function createPlayerNameInputs() {
     const playerDiv = document.createElement("div");
     playerDiv.className = "nameDiv";
     playerDiv.id = "playersNameDiv";
-    playerDiv.innerHTML = ""; // Biztosítjuk, hogy üres legyen
 
+    // Létrehozunk annyi szövegmezőt ahány játékos van
     for (let i = 0; i < JATEKOSOK_SZAMA; i++) {
         const wrapper = document.createElement("div");
         
@@ -36,6 +41,13 @@ function renderPlayerNameInputs() {
         playerDiv.appendChild(wrapper);
     }
 
+    createRoundsInput(playerDiv);
+    createStartButton(playerDiv);
+
+    mainContainer.appendChild(playerDiv);
+}
+// A körök számát beállító input
+function createRoundsInput(playerDiv){
     const round = document.createElement("h2");
     round.innerText = `Körök száma`;
     playerDiv.appendChild(round);
@@ -45,32 +57,38 @@ function renderPlayerNameInputs() {
     roundsInput.value = 5;
     roundsInput.min = 1;
     roundsInput.id = "gameRounds";
-    
     playerDiv.appendChild(roundsInput);
+}
 
+// Játék indító gomb
+function createStartButton(playerDiv){
     const startBtn = document.createElement("input");
     startBtn.type = "button";
     startBtn.value = "Játék indítása";
     startBtn.style.fontSize = "4vw";
     startBtn.onclick = loadPlayers;
     playerDiv.appendChild(startBtn);
-
-    mainContainer.appendChild(playerDiv);
 }
 
 // Segédfüggvény: Üres mezők ellenőrzése
 function hasEmptyPlayerField() {
+    let isAnyEmpty = false;
     const inputs = document.getElementsByClassName("playerInputClass");
+    
     for (let input of inputs) {
-        if (input.value.trim() === "") return true;
+        if (input.value.trim() === "") {
+            input.classList.add("missingName");
+            isAnyEmpty = true;
+        } else {
+            input.classList.remove("missingName");
+        }
     }
-    return false;
+    return isAnyEmpty;
 }
 
 // 2. lépés: A játéktér felépítése
 function loadPlayers() {
     if (hasEmptyPlayerField()) {
-        alert("Hiányzó játékos név!");
         return;
     }
 
@@ -87,7 +105,20 @@ function loadPlayers() {
 
     // Rögzítés gomb létrehozása
     createPinButton();
+    createErrorOutput()
     createRoundDisplay();
+    createRoundResultDisplay()
+
+}
+
+function createRoundResultDisplay(){
+    const pointsH1 = document.createElement("h1");
+    pointsH1.innerText = "Pontszámok"
+    mainContainer.append(pointsH1,document.createElement("hr"))
+    
+    const roundResultDisplay = document.createElement("h1");
+    roundResultDisplay.id = "resultDisplay";
+    mainContainer.appendChild(roundResultDisplay);
 }
 
 function playerDataCreate(index, name){
@@ -99,7 +130,13 @@ function playerDataCreate(index, name){
     }
     playersData.push(newPlayer);    
 }
-
+function createErrorOutput(){
+    const msgDiv = document.createElement("div");
+    msgDiv.className = "errorOutput";
+    msgDiv.innerText = "Hibaüzenet helye";
+    mainContainer.appendChild(msgDiv);
+    
+}
 function createRoundDisplay(){
     const roundDisplay = document.createElement("h1");
     roundDisplay.id = "roundDisplay";
@@ -112,26 +149,34 @@ function updateRoundDisplay(){
     roundDisplay.innerText = `${currRound}/${maxRounds}`;
 }
 
+function updateRoundResultDisplay(){
+    let loser = findLoser();
+    let result = createRoundResult(loser);
+
+    document.getElementById("resultDisplay").innerText = result;
+}
+
+function updateAllPlayersCountDisplay(){
+    const currPointsDisplay = document.getElementsByClassName("pontszam-kijelzo");
+    const totalPointsDisplay = document.getElementsByClassName("osszpontszam-kijelzo"); 
+
+    for(let i = 0; i < currPointsDisplay.length; i++){
+        currPointsDisplay[i].innerText = `Pontszám: ${playersData[i].points}`
+        totalPointsDisplay[i].innerText = `Összesített Pontszám: ${playersData[i].totalPoints}`           
+    }
+}
+
 // Egy játékos táblájának létrehozása
 function createPlayerBoard(index, name) {
     const playerContainer = document.createElement("div");
     playerContainer.classList.add(`jatekos${index}`);
 
-    // Név
-    const nameHeader = document.createElement("h1");
-    nameHeader.innerText = name;
-    nameHeader.classList.add("jatekos-nev");
-
-    // Pontszám kijelző
-    const scoreDisplay = document.createElement("h1");
-    const scoreClass = `osszPont${index}`;
-    scoreDisplay.classList.add(scoreClass, "pontszam-kijelzo");
-    scoreDisplay.innerText = "Pontszám: 0";
-    scoreDisplay.dataset.score = "0"; // Adat attribútumban is tároljuk a könnyebb kezelésért
-
-    // Pont gombok konténere
-    const pointsContainer = document.createElement("div");
-    pointsContainer.classList.add("pontokDiv");
+    // Játékos elemeit tároló tömb
+    let playerElements = [];
+    playerElements.push(createNameElement(name));
+    playerElements.push(createPointsContainerElement());
+    playerElements.push(createCurrentPointsElement());
+    playerElements.push(createTotalPointsElement());
 
     // Gombok generálása (1-20 és 40)
     for (let j = 1; j <= 21; j++) {
@@ -147,15 +192,46 @@ function createPlayerBoard(index, name) {
             this.classList.toggle("kivalasztva");
             
             const isSelected = this.classList.contains("kivalasztva");
-            updateScore(scoreDisplay, pointValue, isSelected, index);
+            updateScore(playerElements[2], playerElements[3], pointValue, isSelected, index);
 
         });
 
-        pointsContainer.appendChild(pointBtn);
+        playerElements[1].appendChild(pointBtn);
     }
 
-    playerContainer.append(nameHeader, pointsContainer, scoreDisplay);
+    playerContainer.append(playerElements[0], playerElements[1], playerElements[2],playerElements[3]);
     mainContainer.append(playerContainer, document.createElement("hr"));
+}
+
+// Név
+function createNameElement(name){
+    const nameHeader = document.createElement("h2");
+    nameHeader.innerText = name;
+    nameHeader.classList.add("jatekos-nev-cim"); 
+    return nameHeader;
+}
+
+// Pont kártya tároló
+function createPointsContainerElement(){
+    const pointsContainer = document.createElement("div");
+    pointsContainer.classList.add("pontokDiv");
+    return pointsContainer;
+}
+
+// Aktuális pont kijelző
+function createCurrentPointsElement(){
+    const currentScoreDisplay = document.createElement("h1");
+    currentScoreDisplay.classList.add("pontszam-kijelzo");
+    currentScoreDisplay.innerText = "Pontszám: 0";
+    return currentScoreDisplay;
+}
+
+// Összesített pont kijelző
+function createTotalPointsElement(){
+    const totalScoreDisplay = document.createElement("h1");
+    totalScoreDisplay.classList.add("osszpontszam-kijelzo");
+    totalScoreDisplay.innerText = "Összesített pontszám: 0";
+    return totalScoreDisplay;
 }
 
 function createPinButton() {
@@ -169,41 +245,59 @@ function createPinButton() {
 }
 
 // Pontszám frissítése
-function updateScore(scoreElement, value, isAdding, ID) {
-    let currentScore = parseInt(scoreElement.dataset.score);
+function updateScore(currentScoreElement,totalScoreElement, value, isAdding, ID) {
+    let currentScore = playersData[ID].points
+    let totalScore = playersData[ID].totalPoints
     const amount = parseInt(value);
 
     // Globális és helyi változók frissítése
     if (isAdding) {
-        globalisPontszam += amount;
         currentScore += amount;
+        totalScore += amount;
         playersData[ID].points += amount;
     } else {
-        globalisPontszam -= amount;
         currentScore -= amount;
+        totalScore -= amount;
         playersData[ID].points -= amount;
     }
-    playersData[ID].totalPoints = currentScore;
+    playersData[ID].totalPoints = totalScore;
 
     console.log(`Játékos: ${playersData[ID].name} Pontjai: ${playersData[ID].points} Összes: ${playersData[ID].totalPoints}`)
 
     // UI és adat attribútum frissítése
-    scoreElement.dataset.score = currentScore;
-    scoreElement.innerText = `Pontszám: ${currentScore}`;
+    currentScoreElement.innerText = `Pontszám: ${currentScore}`;
+    totalScoreElement.innerText = `Összesített Pontszám: ${totalScore}`
     
-    console.log("Összesített globális pont:", globalisPontszam);
+    console.log("Összesített globális pont:", countPlayersPoints());
+}
+
+function countPlayersPoints(){
+    let total = 0;
+    for(let i = 0; i < JATEKOSOK_SZAMA; i++){
+        total += playersData[i].points;
+    }
+    console.log(playersData);
+    return total
+}
+
+function clearPlayersPoints(){
+        for(let i = 0; i < JATEKOSOK_SZAMA; i++){
+        playersData[i].points = 0;
+    }
 }
 
 // Rögzítés és ellenőrzés
 function pin() {
-    // Az eredeti logika megőrizve: 
-    // Ha > 250, hibaüzenet, hogy legyen kisebb.
-    // Ha < 250, hibaüzenet, hogy legyen nagyobb.
-    if (globalisPontszam > CEL_PONTSZAM) {
-        alert("Az összesített pontszám kisebb, mint 250! (Jelenleg: " + globalisPontszam + ")");
+    const totalPoint = countPlayersPoints();
+    const errorBox = document.querySelector(".errorOutput");
+    if(errorBox){
+        errorBox.style.display = "none";
+    }
+    if (totalPoint < CEL_PONTSZAM) {
+        errorOutputText("Az összesített pontszám kisebb, mint 250! (Jelenleg: " + totalPoint + ")");
         return;
-    } else if (globalisPontszam < CEL_PONTSZAM) {
-        alert("Az összesített pontszám nagyobb, mint 250! (Jelenleg: " + globalisPontszam + ")");
+    } else if (totalPoint > CEL_PONTSZAM) {
+        errorOutputText("Az összesített pontszám nagyobb, mint 250! (Jelenleg: " + totalPoint + ")");
         return;
     }
 
@@ -213,39 +307,44 @@ function pin() {
         btn.classList.remove("kivalasztva");
     }
 
-    let isLastRound = currRound == maxRounds;
+    updateRoundResultDisplay();
 
-    if(isLastRound){
+    // Utolsó kör
+    if(currRound == maxRounds){
         gameEnd()
         return
     }
 
-    let loser = findLoser();
-    console.log(loser);
-
-    let result = createRoundResult(loser)
-
     currRound++;
-
     updateRoundDisplay();
-
-    alert(result);
-    globalisPontszam = 0;
+    clearPlayersPoints();
+    updateAllPlayersCountDisplay();
+}
+function errorOutputText(text){
+    const errorBox = document.querySelector(".errorOutput")
+    errorBox.innerText = text;
+    errorBox.style.display = "block";
 }
 // Megkeresi a vesztest a körben
 function findLoser(){
     let loser = playersData[0];
+    let PlayerContainer = document.querySelector(`.jatekos${roundLoserID}`);
+    PlayerContainer.classList.remove("roundLoser");
 
     for(let i = 1; i < playersData.length;i++){
         if(loser.points < playersData[i].points )
             loser = playersData[i];
     }
 
+    playerContainer = document.querySelector(`.jatekos${loser.id}`);
+    playerContainer.classList.add("roundLoser");
+
+    roundLoserID = loser.id;
     return loser;
 }
 // Összeállítja a kör végeredményét
 function createRoundResult(loser){
-    let result = "pontszámok\n";
+    let result = "";
     for(let i = 0; i < playersData.length;i++){
         if(loser == playersData[i]){
             result += `${playersData[i].name} - ${playersData[i].points} Vesztes\n`;
@@ -253,12 +352,11 @@ function createRoundResult(loser){
         else{
             result += `${playersData[i].name} - ${playersData[i].points}\n`;
         }
-
         playersData[i].points = 0;
     }
 return result
 }
-
+// Játék vége
 function gameEnd(){
     let gameLoser = playersData[0];
 
@@ -267,20 +365,15 @@ function gameEnd(){
             gameLoser = playersData[i];
     }
 
-    alert(`Vége a játéknak!\n Vesztes játékos: ${gameLoser.name} pontjai: ${gameLoser.totalPoints}`);
-
+    document.querySelector(`.jatekos${roundLoserID}`).classList.remove("roundLoser");
+    let playerContainer = document.querySelector(`.jatekos${gameLoser.id}`);
+    playerContainer.classList.add("gameLoser");
     let pinBTN = document.getElementById("pinBtn");
     pinBTN.value = "Új játék" ;
     pinBTN.onclick = resetGame;
 
 }
-function resetGame(){
 
+function resetGame(){
     window.location.href = "index.html"
-    /*
-    playersData = []
-    mainContainer.innerHTML = "";
-    renderPlayerNameInputs();
-    globalisPontszam = 0;
-    currRound = 1;*/
 }
